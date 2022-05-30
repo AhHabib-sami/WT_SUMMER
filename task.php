3cbaef969727dceac9c90c825dab299f0d04a85a
AhHabib-sami <82077580+AhHabib-sami@users.noreply.github.com>
2022-05-30 22:14:42 +0600

    Add files via upload

---
+++ task.php
@@ -0,0 +1,42 @@
+
+<html>
+    <head>
+        <title> My Home</title>
+</head>
+<body>
+    <h1> This is home </h1>
+    
+    <form > 
+        
+    <table>
+     <tr> 
+        <td>First name: <input type = "text"> </tr>
+        Last Name: <input type = "text"></td>
+        <tr>
+        <td>Age: <input type = "text"></td> </tr>
+     
+     <tr> <td>Destination
+     <input type = "radio" name = "designation"> Junior Programmer 
+     <input type = "radio" name = "designation"> Senior Programmer 
+     <input type = "radio" name = "designation"> Project Lead </td></tr>
+
+     <tr> 
+        <td><label for email>Email: <input type = "text"></label></td></tr>
+        <tr><label for password><td>Password: <input type = "password"></label></td>
+        </tr>
+   
+     <tr><td>Preferred Language
+    <input type = "checkbox"  >PHP
+    <input type = "checkbox" >JAVA
+    <input type = "checkbox" >C++</td></tr>
+
+
+     <tr><td> Please choose a file <input type = "file"></td></tr>
+        <tr><td><input type = "Submit"></td></tr>
+        <tr><td><input type = "Reset"></td></tr>
+          
+       
+</table>
+</form>
+</body>
+</html>
