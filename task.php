068e5ed7801dacb2b5730235abb4e0d71fdf2d24
AhHabib-sami <82077580+AhHabib-sami@users.noreply.github.com>
2022-06-04 21:39:00 +0600

    Add files via upload

--- task.php
+++ task.php
@@ -6,35 +6,33 @@
 <body>
     <h1> This is home </h1>
     
-    <form > 
-        
-    <table>
-     <tr> 
-        <td>First name: <input type = "text"> </tr>
-        Last Name: <input type = "text"></td>
-        <tr>
-        <td>Age: <input type = "text"></td> </tr>
+    <form action = "../control/ctrl.php " method="REQUEST"> 
+        <table>
+     <tr><td> First Name <input type = "text"  name = "fname"> </td></tr>
+     <tr><td> Last Name <input type = "text" name = "fname2"> </td></tr>
+     <tr> <td>Age    <input type = "text" name = "fname3"></td></tr> 
      
-     <tr> <td>Destination
-     <input type = "radio" name = "designation"> Junior Programmer 
+     
+      <tr> <td>Destination
+     <input type = "radio" name = "designation" > Junior Programmer 
      <input type = "radio" name = "designation"> Senior Programmer 
      <input type = "radio" name = "designation"> Project Lead </td></tr>
 
-     <tr> 
-        <td><label for email>Email: <input type = "text"></label></td></tr>
-        <tr><label for password><td>Password: <input type = "password"></label></td>
-        </tr>
-   
+     <tr> <td>Email: <input type = "text"></td></tr>
+     <tr> <td>Password: <input type = "password" name = "pass"></td></tr>
+    
      <tr><td>Preferred Language
-    <input type = "checkbox"  >PHP
-    <input type = "checkbox" >JAVA
-    <input type = "checkbox" >C++</td></tr>
+    <input type = "checkbox"  name = "ch1" value = "c1">PHP
+     <input type = "checkbox" name = "ch2" value = "c2">JAVA
+    <input type = "checkbox" name = "ch2" value = c3>C++</td></tr>
 
-
-     <tr><td> Please choose a file <input type = "file"></td></tr>
+      <tr> <td>Email:
+       <input type = "text" name = "mail"> </td></tr>
+       
+     <tr><td> Please choose a file <input type = "text" value = "choose file"> No file chosen</td></tr>
         <tr><td><input type = "Submit"></td></tr>
         <tr><td><input type = "Reset"></td></tr>
-          
+      
        
 </table>
 </form>
