d16945657affbaf835a971a6d5d17752ae9f0b1c
AhHabib-sami <82077580+AhHabib-sami@users.noreply.github.com>
2022-06-10 18:17:09 +0600

    Add files via upload

--- ctrl.php
+++ ctrl.php
@@ -1,5 +1,7 @@
 <?php
+
 $name = $_REQUEST["fname"];
+$name2 = $_REQUEST["fname2"];
 if(empty($name))
 {
     echo "<br>Name should be filled ";
@@ -15,35 +17,120 @@ else{
         echo"<br> Please input your second name more than 4 char";
     }
 }
-if(isset ($_REQUEST["ch1"] ) ||isset ($_REQUEST["ch2"] ) || isset ($_REQUEST["ch3"] ))
-{
-   echo "<br>selected" ;
-}
+
+    
+   if (isset($_REQUEST["PHP"]) && isset($_REQUEST["JAVA"]) && isset($_REQUEST["CPP"]))
+    {
+       echo "<br> languages ".$_REQUEST["PHP"]. " and ".$_REQUEST["JAVA"]." and ".$_REQUEST["CPP"];
+       $prefdata = $_REQUEST["PHP"]." and ".$_REQUEST["JAVA"]." and " .$_REQUEST["CPP"];
+    }
+   
+    else if (isset ($_REQUEST["JAVA"]) && isset($_REQUEST["CPP"]))
+    {
+        echo "<br> You have selected ".$_REQUEST["JAVA"]." and ".$_REQUEST["CPP"];
+        $prefdata = $_REQUEST["JAVA"]." and ".$_REQUEST["CPP"];
+
+    }
+    else if (isset ($_REQUEST["PHP"]) && isset($_REQUEST["CPP"]))
+    {
+        echo "<br> You have selected ".$_REQUEST["PHP"]." and ".$_REQUEST["CPP"];
+        $prefdata = $_REQUEST["PHP"]." and ".$_REQUEST["CPP"];
+    }
+    else if (isset ($_REQUEST["PHP"]) && isset($_REQUEST["JAVA"]))
+    {
+        echo "<br> You have selected ".$_REQUEST["PHP"]." and ".$_REQUEST["JAVA"];
+        $prefdata = $_REQUEST["PHP"]." and ".$_REQUEST["JAVA"];
+    }
+    
+    else if(isset($_REQUEST["PHP"])) {
+        echo "<br> You have selected ". $_REQUEST["PHP"];
+        $prefdata = $_REQUEST["PHP"];
+    
+    }
+    else if(isset($_REQUEST["JAVA"]))
+    {
+        echo "<br> You have selected ". $_REQUEST["JAVA"];
+        $prefdata = $_REQUEST["JAVA"];
+    
+
+    }
+    else if (isset($_REQUEST["CPP"]))
+    {
+        echo "<br> You have selected ".$_REQUEST["CPP"];
+        $prefdata = $_REQUEST["CPP"];
+    }
+
 else 
 {
-    echo "<br>Please enter";
-}
-if(isset($_REQUEST["designation"]))
-{
-    $_designation=$_REQUEST["designation"];
-    echo"<br> You have selected designation";
-}
-else{
-    echo "<br>Please choose radio button";
+    echo "<br>Please enter your preferred language";
+    $prefdata="";
 }
 
-if(is_numeric($_REQUEST["pass"]))
+if(isset($_REQUEST["designation"]))
 {
-    echo"<br>You're correct";
+   // $_designation=$_REQUEST["designation"];
+    echo"<br> You have selected  ".$_REQUEST["designation"];
 }
-else 
-{
-    echo "<br>Please enter a password which contains numeric value ";
+else{
+    echo "<br>Please choose designation";
+    $_REQUEST["designation"] ="";
 }
+
+$sami = $_REQUEST["pass"];
+
+if(strlen($sami>4) && is_numeric($sami)){
+    echo "<br>Your password is valid";
+}
+else{
+    echo "<br>Your password must be more than 4 char with numeric";
+}
+
+
+//if(isset($_REQUEST["ch1"]) && isset($_REQUEST["ch2"]) && (isset($_REQUEST["ch3"] )))
+//{
+ //   echo "<br> You have selected ".$_REQUEST["ch1"]. "and" .$_REQUEST["ch2"]. "and". $_REQUEST["ch3"];
+//}
+
+
+
+
+
+
 if(empty($_REQUEST["mail"]))
 {
     echo"<br> enter mail";
 }
 else {
-    echo "<br>Ok for mail";
+  
+    echo "<br>Your mail id is ".$_REQUEST["mail"];
 }
+//Declaration for preferred langages
+
+echo "<br>".$_FILES["myfile"]["name"];
+if(move_uploaded_file($_FILES["myfile"]["tmp_name"],"../upload/".$_FILES["myfile"]["name"]))
+{
+    echo "File uploaded";
+}
+else{
+    echo "File not uploaded";
+}
+    
+
+$mydata =  array
+(
+    'firstname' => $name,
+    'second name' => $name2,
+    'password' => $_REQUEST["pass"],
+    'E-mail' => $_REQUEST["mail"],
+    'Preferred Languages' => $prefdata,
+    'FILE'=>$_FILES["myfile"]["name"],
+    'Designation' => $_REQUEST["designation"]
+    
+
+);
+$jsondata=json_encode($mydata, JSON_PRETTY_PRINT);
+if(file_put_contents("../data/data.json",$jsondata)){
+    echo "<br>data saved";
+}
+
+?>
--- task.php
+++ task.php
@@ -1,37 +1,44 @@
-
+<?php
+include("../control/ctrl.php")
+?>
 <html>
     <head>
-        <title> My Home</title>
+        <title> task</title>
 </head>
 <body>
-    <h1> This is home </h1>
+    <h1> Welcome to the registration form </h1>
     
-    <form action = "../control/ctrl.php " method="REQUEST"> 
-        <table>
+    <form action="" method="post" enctype="multipart/form-data">   
+        
+    <table>
      <tr><td> First Name <input type = "text"  name = "fname"> </td></tr>
      <tr><td> Last Name <input type = "text" name = "fname2"> </td></tr>
      <tr> <td>Age    <input type = "text" name = "fname3"></td></tr> 
      
      
-      <tr> <td>Destination
-     <input type = "radio" name = "designation" > Junior Programmer 
-     <input type = "radio" name = "designation"> Senior Programmer 
-     <input type = "radio" name = "designation"> Project Lead </td></tr>
+      <tr> <td>Designation
+     <input type = "radio" name = "designation" value = "junior programmer" > Junior Programmer 
+     <input type = "radio" name = "designation" value = "senior programmer"> Senior Programmer 
+     <input type = "radio" name = "designation" value = "project lead"> Project Lead </td></tr>
 
-     <tr> <td>Email: <input type = "text"></td></tr>
+     <tr> <td>Email:
+    <input type = "text" name = "mail"> </td></tr>
+
      <tr> <td>Password: <input type = "password" name = "pass"></td></tr>
     
      <tr><td>Preferred Language
-    <input type = "checkbox"  name = "ch1" value = "c1">PHP
-     <input type = "checkbox" name = "ch2" value = "c2">JAVA
-    <input type = "checkbox" name = "ch2" value = c3>C++</td></tr>
+    <input type = "checkbox"  name = "PHP" value = "PHP">PHP
+     <input type = "checkbox" name = "JAVA" value = "JAVA">JAVA
+    <input type = "checkbox"  name =  "CPP" value = "CPP">C++</td></tr>
 
-      <tr> <td>Email:
-       <input type = "text" name = "mail"> </td></tr>
+      
        
-     <tr><td> Please choose a file <input type = "text" value = "choose file"> No file chosen</td></tr>
-        <tr><td><input type = "Submit"></td></tr>
-        <tr><td><input type = "Reset"></td></tr>
+      <br><tr><td>Please choose file 
+      <br><input type="file"name="myfile"></td></tr>
+
+
+      <tr><td><input type = "Submit"></td></tr>
+      <br><tr><td><input type = "Reset"></td></tr>
       
        
 </table>
